04da88aa92a06f4513b2c16355c58af08d0e0882
Jonathan Libonati (V) <jlibonati@apple.com>
2025-10-26 02:55:06 -0300

    first commit

---
+++ src/data/contactData.js
@@ -0,0 +1,33 @@
+
+
+export const contactData = {
+  telefonos: {
+    label: "Telefonos",
+    movil: "11‑3475‑9984",
+    rawMovil: "5491134759984",
+  },
+  correo: {
+    label: "Correo electrónico",
+    direccion: "info@axiabaires.com",
+  },
+  sitioWeb: {
+    label: "Sitio Web",
+    url: "www.paroissien4646.com"
+  },
+};
+
+export const contactDataList = [
+  {
+    label: contactData.telefonos.label,
+    movil: contactData.telefonos.movil,
+  },
+  {
+    label: contactData.correo.label,
+    direccion: contactData.correo.direccion,
+  },
+  {
+    label: contactData.sitioWeb.label,
+    url: contactData.sitioWeb.url,
+  },
+]
+
